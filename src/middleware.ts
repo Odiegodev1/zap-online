3939532caf58c3fdab3f91260f90b4de6b6e7d22
Odiegodev1 <odiegodev10@gmail.com>
2025-07-24 07:24:54 -0300

    Atualiza erro midddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import { handleGetUser } from "@/lib/server/auth";
 
-export async function middleware(request: NextRequest) {
-  const user = await handleGetUser(); // obtenha o usuário corretamente
-
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value;
   const { pathname } = request.nextUrl;
 
-  // Se não estiver em /auth e não tiver user, redirecione para login
-  if (!pathname.startsWith('/auth') && !user) {
+  // Se não estiver em /auth e não tiver token, redirecione para login
+  if (!pathname.startsWith('/auth') && !token) {
     return NextResponse.redirect(new URL('/auth/signin', request.url));
   }
 
-  // Se estiver em /auth e tiver user, redirecione para home
-  if (pathname.startsWith('/auth') && user) {
+  // Se estiver em /auth e tiver token, redirecione para home
+  if (pathname.startsWith('/auth') && token) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
-  // Caso contrário, deixe passar normalmente (retorne undefined ou NextResponse.next())
   return NextResponse.next();
 }
 
