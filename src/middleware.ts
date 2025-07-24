66f7b28ab7981563b106a9af6d3f6d70a85996fd
Odiegodev1 <odiegodev10@gmail.com>
2025-07-24 07:30:13 -0300

    Atualiza erro midddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value;
+  const token = request.cookies.get(`${process.env.NEXT_PUBLIC_AUTH_KEY}`)?.value;
   const { pathname } = request.nextUrl;
 
-  // Se não estiver em /auth e não tiver token, redirecione para login
   if (!pathname.startsWith('/auth') && !token) {
     return NextResponse.redirect(new URL('/auth/signin', request.url));
   }
 
-  // Se estiver em /auth e tiver token, redirecione para home
   if (pathname.startsWith('/auth') && token) {
     return NextResponse.redirect(new URL('/', request.url));
   }
